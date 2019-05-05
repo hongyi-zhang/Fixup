be6a85a9cfb88f678bacca88cffac1bf787f882a
hongyi-zhang <hongyi.zhang.pku@gmail.com>
2019-05-05 01:10:52 -0700

    transformer + fixup on IWSLT

---
+++ fairseq/fairseq/modules/__init__.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
+
+from .adaptive_softmax import AdaptiveSoftmax
+from .beamable_mm import BeamableMM
+from .character_token_embedder import CharacterTokenEmbedder
+from .conv_tbc import ConvTBC
+from .downsampled_multihead_attention import DownsampledMultiHeadAttention
+from .grad_multiply import GradMultiply
+from .highway import Highway
+from .learned_positional_embedding import LearnedPositionalEmbedding
+from .linearized_convolution import LinearizedConvolution
+from .multihead_attention import MultiheadAttention
+from .scalar_bias import ScalarBias
+from .sinusoidal_positional_embedding import SinusoidalPositionalEmbedding
+
+from .multihead_attention_zero import MultiheadAttentionZero
+
+__all__ = [
+    'AdaptiveSoftmax',
+    'BeamableMM',
+    'CharacterTokenEmbedder',
+    'ConvTBC',
+    'DownsampledMultiHeadAttention',
+    'GradMultiply',
+    'Highway',
+    'LearnedPositionalEmbedding',
+    'LinearizedConvolution',
+    'MultiheadAttention',
+    'ScalarBias',
+    'SinusoidalPositionalEmbedding',
+    'MultiheadAttentionZero'
+]
